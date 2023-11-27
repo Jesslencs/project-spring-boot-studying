44eea6e116a125755ea1469ecd8c6ac88e5d9a37
Jessica Alencar <jessicacontatoalencar@gmail.com>
2023-11-27 13:44:46 -0300

    finishing the course

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -27,4 +28,30 @@ public class StudentService {
 
     }
 
-}
+    public void addNewStudent(Student student) {
+       Optional<Student> studentOptional = studentRepository
+               .findStudentByEmail(student.getEmail());
+       if(studentOptional.isPresent()){
+           throw  new IllegalStateException("email taken");
+
+
+       }
+       studentRepository.save(student);
+
+
+
+    }
+
+    public void deleteStudent(Long studentId) {
+        studentRepository.findById(studentId);
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exists");
+
+
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+
+    }
